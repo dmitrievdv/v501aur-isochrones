20188e68008b15d850df49c528876940207ac4ec
dmitrievdv <dmitrievdv242@gmail.com>
2025-10-22 16:11:15 +0300

    kurucz-tess-phot

---
+++ kurucz-int.jl
@@ -0,0 +1,138 @@
+function find_near_nodes(x, nodes)
+    x_2 = findfirst(y -> y ≥ 0.0, nodes .- x)
+    if x_2 == 1
+        return 2, 1
+    end
+    if (isnothing(x_2)) #& ((nodes[end] - x) ≥ -1e-3)
+        return length(nodes), length(nodes) - 1
+    end 
+    return x_2 - 1, x_2
+end
+
+function linear_interpolation(x, x_1, x_2, y_1, y_2)
+    return y_1 + (x - x_1)*(y_2 - y_1)/(x_2 - x_1)
+end
+
+function interpolate_kurucz(T_eff, logg)
+    logg_nodes = [0.0:0.5:5.0;]
+    T_eff_nodes = [3500:250.0:9750; 10000:5e2:12500; 13000:1e3:35000]
+
+    i_T_eff_1, i_T_eff_2 = find_near_nodes(T_eff, T_eff_nodes)
+    i_logg_1, i_logg_2 = find_near_nodes(logg, logg_nodes)
+
+    kurucz_fits_1 = FITS("kurucz-p00/kp00_$(round(Int, T_eff_nodes[i_T_eff_1])).fits")
+    kurucz_fits_2 = FITS("kurucz-p00/kp00_$(round(Int, T_eff_nodes[i_T_eff_2])).fits")
+
+    λs_1 = read(kurucz_fits_1[2], "WAVELENGTH") 
+    λs_2 = read(kurucz_fits_2[2], "WAVELENGTH")
+
+    int_g1 = round(Int, logg_nodes[i_logg_1]*10)
+    int_g2 = round(Int, logg_nodes[i_logg_2]*10)
+
+    int_g1_str = @sprintf "%02i" int_g1
+    int_g2_str = @sprintf "%02i" int_g2
+
+    kurucz_spec_11 = read(kurucz_fits_1[2], "g$int_g1_str")
+    kurucz_spec_12 = read(kurucz_fits_1[2], "g$int_g2_str")
+
+    kurucz_spec_21 = read(kurucz_fits_2[2], "g$int_g1_str")
+    kurucz_spec_22 = read(kurucz_fits_2[2], "g$int_g2_str")
+
+    kurucz_spec_1 = linear_interpolation(logg, logg_nodes[[i_logg_1, i_logg_2]]..., kurucz_spec_11, kurucz_spec_12)
+    kurucz_spec_2 = linear_interpolation(logg, logg_nodes[[i_logg_1, i_logg_2]]..., kurucz_spec_21, kurucz_spec_22)
+
+    if !isnothing(findfirst(x -> abs(x) > 1e-3, λs_1 .- λs_2))
+        kurucz_spec_2_spl = Spline1D(λs_2, kurucz_spec_2, k = 1)
+        kurucz_spec_2 .= kurucz_spec_2_spl.(λs_1)
+    end
+        
+    kurucz_spec = linear_interpolation(T_eff, T_eff_nodes[[i_T_eff_1, i_T_eff_2]]..., kurucz_spec_1, kurucz_spec_2)
+    return λs_1, kurucz_spec
+end
+
+function read_tess_response_spl()
+    tess_response_df = CSV.read("tess-response-function-v2.0.csv", DataFrame, comment = "#")
+    tess_response_spl = Spline1D(tess_response_df[:,1] * 10, tess_response_df[:,2], k = 1)
+end
+
+function calc_tess_kurucz_flux_no_extinction(T_eff, logg, tess_response_spl)
+    λs, kurucz_spec = interpolate_kurucz(T_eff, logg)
+# wls, flux, cntm = Korg.synth(Teff = T_eff,
+#                              logg = logg, 
+#                              m_H = 0.0, 
+#                              wavelengths = Korg.Wavelengths(5000, 12000, 1), 
+#                              linelist = Korg.read_linelist("gfallvac08oct17.dat", format = "kurucz_vac"), 
+#                              R = 100)
+
+    Δλs_kurucz = λs[2:end] - λs[1:end-1]
+    λs_mid_kurucz = (λs[2:end] + λs[1:end-1])/2
+    kurucz_spec_mid = (kurucz_spec[2:end] + kurucz_spec[1:end-1])/2
+    return sum(@. kurucz_spec_mid * tess_response_spl(λs_mid_kurucz) * Δλs_kurucz)
+end
+
+
+function calc_tess_kurucz_flux_no_extinction(T_eff, logg)
+    tess_response_spl = read_tess_response_spl()
+    calc_tess_kurucz_flux_no_extinction(T_eff, logg, tess_response_spl)
+end
+
+extinct_law(λ, Av) = Av/(2.5*log10(ℯ))/(λ/5e3)
+
+function calc_tess_kurucz_flux_with_extinction(T_eff, logg, Av, tess_response_spl)
+    λs, kurucz_spec = interpolate_kurucz(T_eff, logg)
+# wls, flux, cntm = Korg.synth(Teff = T_eff,
+#                              logg = logg, 
+#                              m_H = 0.0, 
+#                              wavelengths = Korg.Wavelengths(5000, 12000, 1), 
+#                              linelist = Korg.read_linelist("gfallvac08oct17.dat", format = "kurucz_vac"), 
+#                              R = 100)
+
+    Δλs_kurucz = λs[2:end] - λs[1:end-1]
+    λs_mid_kurucz = (λs[2:end] + λs[1:end-1])/2
+    kurucz_spec_mid = (kurucz_spec[2:end] + kurucz_spec[1:end-1])/2
+    return sum(@. kurucz_spec_mid * tess_response_spl(λs_mid_kurucz) * exp(-extinct_law(λs_mid_kurucz, Av)) * Δλs_kurucz)
+end
+
+function calc_tess_kurucz_flux_with_extinction(T_eff, logg, Av)
+    tess_response_spl = read_tess_response_spl()
+    calc_tess_kurucz_flux_with_extinction(T_eff, logg, Av, tess_response_spl)
+end
+
+# Av = 0.54 * 3.1
+
+# tess_rel_no_Av = calc_tess_kurucz_flux_no_extinction(7800, 4.2)/calc_tess_kurucz_flux_no_extinction(4700, 2.2)*1.5^2/26.6^2
+# tess_rel_Av = calc_tess_kurucz_flux_with_extinction(7800, 4.2, Av)/calc_tess_kurucz_flux_with_extinction(4700, 2.2, Av)*1.5^2/26.6^2
+
+# logg_grid = [1.5:0.1:5.0;]
+# T_eff_grid = [3500:1e2:10000;]
+
+# n_logg = length(logg_grid)
+# n_T_eff = length(T_eff_grid)
+
+# tess_rel_Av_arr = zeros(n_logg, n_T_eff)
+# tess_rel_arr = zeros(n_logg, n_T_eff)
+
+# for (i_logg, logg) in enumerate(logg_grid)
+#     for (i_T_eff, T_eff) in enumerate(T_eff_grid)
+#         if (T_eff > 9250) & (logg < 2.0)
+#             tess_rel_arr[i_logg, i_T_eff] = -1.0
+#             tess_rel_Av_arr[i_logg, i_T_eff] = -1.0
+#         else
+#             tess_rel_arr[i_logg, i_T_eff] = calc_tess_kurucz_flux_no_extinction(T_eff, logg)
+#             tess_rel_Av_arr[i_logg, i_T_eff] = calc_tess_kurucz_flux_with_extinction(T_eff, logg, Av)
+#         end
+#     end
+# end
+
+# mkpath("tess-tables/")
+# for (i_T_eff, T_eff) in enumerate(T_eff_grid)
+#     open("tess-tables/tess_flux_$(round(Int, T_eff)).dat", "w") do io
+#         println(io, "# TESS fluxes for T_eff = $(round(Int, T_eff)).")
+#         println(io, "# Fluxes are in arbitrary units, meant to only be used in relation to each other.")
+#         println(io, "# Negative numbers mean that this (T_eff, logg) combination is invalid for Kurucz, 1993 models.")
+#         println(io, "# Columns: logg, flux, flux with extinction. \n")
+#         for i_logg in 1:n_logg
+#             @printf io "%5f %10e %10e \n" logg_grid[i_logg] tess_rel_arr[i_logg, i_T_eff] tess_rel_Av_arr[i_logg, i_T_eff]
+#         end
+#     end
+# end
--- main.jl
+++ main.jl
@@ -1,11 +1,18 @@
 using Makie
 using GLMakie
-using DataFrames
+using DataFrames, CSV
 using Polynomials
 using LaTeXStrings
+using Printf
+using FITSIO
+using Dierckx
 
-star_masses = [2.5:0.25:7.0;]
+
+include("kurucz-int.jl")
+
+star_masses = [1:0.15:7;]
 mesa_dir = "mesa-data"
+mesa_processed_dir = "mesa-data-processed"
 
 function get_mesa_df(star_mass)
     mesa_file_name = "track_M$(star_mass)_Z0.0147_ROT0.00_history.data"
@@ -18,10 +25,39 @@ function get_mesa_df(star_mass)
     mesa_df = DataFrame([Pair(name_col_tuple...) for name_col_tuple in zip(col_names, cols)])
 end
 
+function get_mesa_df_processed(star_mass)
+    star_mass_str = @sprintf "%.2f" star_mass
+    mesa_file_name = "ROT0.00M$(star_mass_str)Z0.0147.csv"
+
+    CSV.read("$mesa_processed_dir/$mesa_file_name", DataFrame)
+end
+
+function get_mesa_df_processed(star_mass, tess_response_spl, Av)
+    star_mass_str = @sprintf "%.2f" star_mass
+    mesa_file_name = "ROT0.00M$(star_mass_str)Z0.0147.csv"
+
+    mesa_df = CSV.read("$mesa_processed_dir/$mesa_file_name", DataFrame)
+    n_mesa = nrow(mesa_df)
+
+    log_tess_luminosity = zeros(n_mesa)
+
+    for i_mesa = 1:n_mesa
+        log_tess_luminosity[i_mesa] = try
+            log10(mesa_df.radius[i_mesa]^2 * calc_tess_kurucz_flux_with_extinction(mesa_df.Teff[i_mesa], mesa_df.log_g[i_mesa], Av, tess_response_spl))
+        catch 
+            -1e2
+        end
+    end
+    # println(log_tess_luminosity)
+
+    mesa_df[!, :log_TESS] = log_tess_luminosity
+    mesa_df
+end
+
 function get_age_df(mesa_df, lg_age)
     lg_ages = log10.(mesa_df.star_age)
     i_age = findfirst(x -> x > lg_age, lg_ages)
-    println(i_age)
+    # println(i_age)
 
     if isnothing(i_age)
         df = DataFrame([Pair(name_val_tuple...) for name_val_tuple in zip(names(mesa_df), collect(mesa_df[end,:]))])
@@ -108,39 +144,51 @@ end
 
 function find_secondary_mass(mass_function, primary_mass)
     mass_poly = Polynomial([-mass_function*primary_mass^2, -2mass_function*primary_mass, -mass_function, 1])
-    mass_poly_roots = roots(mass_poly)
+    mass_poly_roots = Polynomials.roots(mass_poly)
     return real.(mass_poly_roots[abs.(imag.(mass_poly_roots)) .≈ 0.0])[1]
 end
 
-mesa_dfs = get_mesa_df.(star_masses)
-lg_ages = [7.5:0.1:8.5;]
+tess_response_spl = read_tess_response_spl()
+Av = 0.54*3.1
+mesa_dfs = get_mesa_df_processed.(star_masses, Ref(tess_response_spl), Av)
+lg_ages = [7.5:0.1:8.9;]
 
 luminosity_rel = 100
-mass_function = 0.4
+mass_function = 0.1373
+
 
 isochrone_dfs = get_isochrone_df.(Ref(mesa_dfs), lg_ages)
 
 fig_binary = Figure()
-ax_mass = Axis(fig_binary[1,1], xlabel = L"M_1", ylabel = L"\lg L_1 - \lg L_2")
-ax_logg = Axis(fig_binary[1,2], xlabel = L"\lg g", ylabel = L"\lg L_1 - \lg L_2")
-ax_teff = Axis(fig_binary[1,3], xlabel = L"T_\mathrm{eff}", ylabel = L"\lg L_1 - \lg L_2")
+ax_mass = Axis(fig_binary[1,1], xlabel = L"M_1", ylabel = L"\lg F_1 - \lg F_2")
+ax_2mass = Axis(fig_binary[1,2], xlabel = L"M_2", ylabel = L"\lg F_1 - \lg F_2")
+ax_logg = Axis(fig_binary[2,1], xlabel = L"\lg g", ylabel = L"\lg F_1 - \lg F_2")
+ax_teff = Axis(fig_binary[2,2], xlabel = L"T_\mathrm{eff}", ylabel = L"\lg F_1 - \lg F_2")
+
+
 
 for (i_age, lg_age) in enumerate(lg_ages)
     isochrone_df = isochrone_dfs[i_age]
     isochrone_df.log_L[isochrone_df.model_number .< 0] .= NaN
-    primary_masses = [2.25:0.25:7.0;]
+    primary_masses = [2.25:0.01:7.0;]
     secondary_masses = find_secondary_mass.(mass_function, primary_masses)
     primary_dfs = foldl(vcat, interpolate_isochrone_df.(Ref(isochrone_df), primary_masses))
     secondary_dfs = foldl(vcat, interpolate_isochrone_df.(Ref(isochrone_df), secondary_masses))
+
+    lg_TESS_primary = primary_dfs.log_TESS
+    lg_TESS_secondary = secondary_dfs.log_TESS
     lg_L_primary = primary_dfs.log_L
     lg_L_secondary = secondary_dfs.log_L
+
     Δlg_L = lg_L_primary - lg_L_secondary
+    Δlg_TESS = lg_TESS_primary - lg_TESS_secondary
     if isempty(Δlg_L[@. !isnan(Δlg_L)])
         continue
     end
-    lines!(ax_mass, primary_dfs.star_mass, Δlg_L, label = "lg t = $lg_age")
-    lines!(ax_logg, primary_dfs.log_g, Δlg_L)
-    lines!(ax_teff, primary_dfs.Teff, Δlg_L)
+    lines!(ax_mass, primary_dfs.star_mass, Δlg_TESS, label = "lg t = $lg_age")
+    lines!(ax_2mass, secondary_dfs.star_mass, Δlg_TESS, label = "lg t = $lg_age")
+    lines!(ax_logg, primary_dfs.log_g, Δlg_TESS)
+    lines!(ax_teff, primary_dfs.Teff, Δlg_TESS)
 end
 axislegend(ax_mass)
 
@@ -148,16 +196,18 @@ axislegend(ax_mass)
 
 fig = Figure()
 ax_tracks = Axis(fig[1,1], xreversed = true)
+ax_age = Axis(fig[1,3])
 ax_iso = Axis(fig[1,2])
 
 for (i_mass, star_mass) in enumerate(star_masses)
     lines!(ax_tracks, mesa_dfs[i_mass].Teff, mesa_dfs[i_mass].log_L, label = "M = $star_mass")
+    lines!(ax_age, mesa_dfs[i_mass].star_mass, log10.(mesa_dfs[i_mass].star_age))
 end
 
 for (i_age, lg_age) in enumerate(lg_ages)
     isochrone_df = isochrone_dfs[i_age]
     isochrone_df.log_L[isochrone_df.model_number .< 0] .= NaN
-    masses = [2.25:0.01:7.0;]
+    masses = star_masses
     lg_L = interpolate_isochrone_lg_L.(Ref(isochrone_df), masses)
     lines!(ax_iso, masses, lg_L, label = "lg t = $lg_age")
 end
